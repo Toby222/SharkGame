c8a7bc4c4cf30e475e9082aaa0ad647686ab1230
Biggest-Brian <not.a.cool.brazilian@gmail.com>
2026-01-11 02:12:45 -0300

    tabela --> planilha

--- js/data/aspects.js
+++ js/data/aspects.js
@@ -508,7 +508,7 @@ SharkGame.Aspects = {
             tree.handleClickedAspect(this);
         },
         apply(when) {
-            if (when === "levelUp" && SharkGame.Settings.current.doAspectTable === "tabela") {
+            if (when === "levelUp" && SharkGame.Settings.current.doAspectTable === "planilha") {
                 SharkGame.Button.makeButton("respecModeButton", "respec mode", $("#aspectTreeNavButtons"), tree.toggleRefundMode);
                 SharkGame.Button.makeButton("respecButton", "respec all", $("#aspectTreeNavButtons"), () => {
                     if (confirm("Tem certeza que queres vender TODOS os aspectos reembolsáveis?")) {

b6ef9399bb870ff8184f43c35751c8b557ffa7c9
Biggest-Brian <not.a.cool.brazilian@gmail.com>
2026-01-17 00:31:13 -0300

    test log.addMessage

--- js/settings.js
+++ js/settings.js
@@ -210,7 +210,7 @@ SharkGame.Settings = {
         options: ["english", "português"],
         onChange() {
             if (SharkGame.Settings.current.language === "português") {
-                return "it works";
+                log.addMessage("it works");
             }
         },
     },

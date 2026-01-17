33d6968fb0b4603d085fdc2192db285db9dcd167
Biggest-Brian <not.a.cool.brazilian@gmail.com>
2026-01-17 00:21:31 -0300

    testar if

--- js/settings.js
+++ js/settings.js
@@ -209,6 +209,9 @@ SharkGame.Settings = {
         category: "ACCESSIBILITY",
         options: ["english", "português"],
         onChange() {
+            if (SharkGame.Settings.current.language === "português") {
+                return "it works"
+            }
         },
     },
 

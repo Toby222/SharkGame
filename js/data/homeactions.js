7542cb349c6091eaa1d026d253c5df1fb7619674
Three Eels in a Jellyfish Coat <M2.Fire@outlook.com>
2026-02-01 17:31:30 +0400

    -rixed grammar problem

--- js/data/homeactions.js
+++ js/data/homeactions.js
@@ -5073,7 +5073,7 @@ SharkGame.HomeActions = {
         },
 
         getShoveler: {
-            name: "Instruct a ray shoveler",
+            name: "Instruct shoveler ray",
             effect: {
                 resource: {
                     shoveler: 1,
